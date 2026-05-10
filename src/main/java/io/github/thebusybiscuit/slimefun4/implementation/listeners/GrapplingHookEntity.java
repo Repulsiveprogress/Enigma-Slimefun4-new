63c514dc0728936a0ad561dfb52588922f11bcd6
JINJA AAA PATRICAIAA <lololomivk@gmail.com>
2026-05-10 16:35:53 +0300

    Refactor GrapplingHookEntity to return grappling hook to player's inventory instead of dropping it

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/GrapplingHookEntity.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/GrapplingHookEntity.java
@@ -7,24 +7,25 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 
 final class GrapplingHookEntity {
 
-    private final boolean dropItem;
+    private final boolean returnItem;
     private final boolean wasConsumed;
     private final Arrow arrow;
     private final Entity leashTarget;
+    private final Player player;
 
     @ParametersAreNonnullByDefault
     GrapplingHookEntity(Player p, Arrow arrow, Entity leashTarget, boolean dropItem, boolean wasConsumed) {
         this.arrow = arrow;
         this.wasConsumed = wasConsumed;
         this.leashTarget = leashTarget;
-        this.dropItem = p.getGameMode() != GameMode.CREATIVE && dropItem;
+        this.returnItem = p.getGameMode() != GameMode.CREATIVE && dropItem;
+        this.player = p;
     }
 
     @Nonnull
@@ -33,10 +34,9 @@ final class GrapplingHookEntity {
     }
 
     public void drop(@Nonnull Location l) {
-        // If a grappling hook was consumed, drop one grappling hook on the floor
-        if (dropItem && wasConsumed) {
-            Item item = l.getWorld().dropItem(l, SlimefunItems.GRAPPLING_HOOK.item());
-            item.setPickupDelay(16);
+        // If a grappling hook was consumed, return it to the player's inventory
+        if (returnItem && wasConsumed) {
+            player.getInventory().addItem(SlimefunItems.GRAPPLING_HOOK.item());
         }
     }
 
